4cd10ef53f82beac50bd6e7c3b708f8862081caa
Ali Reza <99729607+AliReza1083@users.noreply.github.com>
2022-07-25 09:00:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+import fetch from 'node-fetch';
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+
+let api = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=f88e5d58f9da413209e8ccd1c634eeb1';
+
+//* Dark
+app.get('/D-weatherapi', (req, res) => {
+    fetch(api)
+        .then((val) => {
+            return val.json()
+        })
+        .then((data) => {
+            // const cDegree = Math.floor(data.main.temp - 273.15);
+            if (data.cod == '404') {
+                res.render('dark', {data, convertTimestamp, randImg});
+            } else {
+                console.log(data)
+                res.render('dark', {data, convertTimestamp, randImg});
+            }
+        })
+})
+
+app.post('/D-weatherapi', (req, res) => {
+    const {search} = req.body;
+    console.log(search);
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=f88e5d58f9da413209e8ccd1c634eeb1`
+    res.redirect('/D-weatherapi')
+})
+
+
+//* light
+app.get('/L-weatherapi', (req, res) => {
+    fetch(api)
+        .then((val) => {
+            return val.json()
+        })
+        .then((data) => {
+            // const cDegree = Math.floor(data.main.temp - 273.15);
+            if (data.cod == '404') {
+                res.render('light', {data, convertTimestamp, randImg});
+            } else {
+                res.render('light', {data, convertTimestamp, randImg});
+            }
+        })
+})
+
+app.post('/L-weatherapi', (req, res) => {
+    const {search} = req.body;
+    console.log(search);
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=f88e5d58f9da413209e8ccd1c634eeb1`
+    res.redirect('/L-weatherapi')
+})
+
+
+//* page 2
+let api2 = 'https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=f88e5d58f9da413209e8ccd1c634eeb1';
+
+app.get('/weatherapi/datas', (req, res) => {
+    fetch(api2)
+        .then((val) => {
+            return val.json()
+        })
+        .then((data) => {
+            // const cDegree = Math.floor(data.main.temp - 273.15);
+            if (data.cod == '404') {
+                res.render('apiData', {data, randImg})
+            } else {
+                res.render('apiData', {data, randImg})
+            }
+        })
+})
+
+app.post('/weatherapi/datas', (req, res) => {
+    const {search} = req.body;
+    api2 = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=f88e5d58f9da413209e8ccd1c634eeb1`;
+    res.redirect('/weatherapi/datas')
+})
+
+
+
+
+//! FUNCTIONS
+const imgs = ['img/clear.png', 'img/clear2.png', 'img/cloudy and rainy.png', 'img/cloudy.png', 'img/full moon.png', 'img/heavyrain and storm.png', 'img/night cloudy.png', 'img/night rain.png', 'img/night.png', 'img/rainy.png', 'img/sunny.png', 'img/sunny2.png', 'img/thunder.png']
+const randImg = (path) => {
+    const rand = Math.floor(Math.random() * imgs.length);
+    return `/${imgs[rand]}`;
+}
+
+function convertTimestamp(timestamp) {
+    var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+    yyyy = d.getFullYear(),
+    mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+    dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+    hh = d.getHours(),
+    h = hh,
+    min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+    ampm = 'AM',
+    time;
+            
+    if (hh > 12) {
+        h = hh - 12;
+        ampm = 'PM';
+    } else if (hh === 12) {
+        h = 12;
+        ampm = 'PM';
+    } else if (hh == 0) {
+        h = 12;
+    }
+    
+    // ie: 2013-02-18, 8:35 AM	
+    time = h + ':' + min + ' ' + ampm;
+        
+    return time;
+  }
+
+
+
+app.listen(port, () => {
+    console.log(`Listening to ${port}`);
+})
